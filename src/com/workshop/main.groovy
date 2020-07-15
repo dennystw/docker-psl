ffdfb3eab739929adc6c184de290d50ac2516fbc
dimasrizkyhp <dimas.rizky.hp@gmail.com>
2020-07-15 18:44:24 +0700

    stable

--- src/com/workshop/main.groovy
+++ src/com/workshop/main.groovy
@@ -113,7 +113,7 @@ def main(script) {
         }
 
         stage('Service Healthcheck') {
-            def hostIp = sh script: "ip route show | awk '/default/ {print \$3}'"
+            def hostIp = sh script: "ip route show | awk '/default/ {print \$3}'", returnStdout: true
             def response =  sh script: "curl ${hostIp}:${app_port}/ping", returnStdout: true
             if (response != "pong!"){
                 error("ERROR102 - Service is Unhealthy")
