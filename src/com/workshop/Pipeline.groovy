db654519c1274fe8e03cd3f46f113e9e56c3a50a
dimasrizkyhp <dimas.rizky.hp@gmail.com>
2020-07-16 13:33:15 +0700

    add pr capabilities

---
+++ src/com/workshop/Pipeline.groovy
@@ -0,0 +1,29 @@
+package com.workshop
+
+class Pipeline implements Serializable {
+
+    String repository_name
+    String branch_name
+    String git_user
+    String app_port
+    String pr_num
+    String dockerTool
+
+    Pipeline(
+        String repository_name,
+        String branch_name,
+        String git_user,
+        String app_port,
+        String pr_num,
+        String dockerTool
+    ){
+        this.repository_name = repository_name
+        this.branch_name = branch_name
+        this.git_user = git_user
+        this.app_port = app_port
+        this.pr_num = pr_num
+        this.dockerTool = dockerTool
+    }
+
+}
+
--- src/com/workshop/stages/postbuild.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/groovy
-package com.workshop.stages
-
-def pushRegistry() {
-    println("Push bosq")
-}
