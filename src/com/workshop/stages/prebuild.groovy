859d6162e0740b4d22ce94f82561942346e5db93
Ubuntu <ubuntu@ubuntu1804.linuxvmimages.local>
2020-07-21 01:41:24 -0400

    add content

--- src/com/workshop/stages/prebuild.groovy
+++ src/com/workshop/stages/prebuild.groovy
@@ -16,9 +16,9 @@ def details(Pipeline p) {
 
 def checkPR(Pipeline p) {
     def git_pr_response = httpRequest authentication: 'github-personal', url: 'https://api.github.com/repos/dimasmamot/tkpd-demo/pulls/1', wrapAsMultipart: false
-    println("${git_pr_response.toString()}")
+    println("${git_pr_response}")
 
-    def parsed_git_pr_response = readJSON text: "${git_pr_response.toString()}"
+    def parsed_git_pr_response = readJSON text: "${git_pr_response}"
     println("Git Base : ${parsed_git_pr_response['base']['ref']}")
 }
 
