23d21a13e520f60532fb0fb7d3297532172ad92a
Denny Setiawan <dennystw@outlook.com>
2020-07-25 12:14:31 +0700

    Update Pipeline.groovy

--- src/com/workshop/Pipeline.groovy
+++ src/com/workshop/Pipeline.groovy
@@ -5,6 +5,7 @@ class Pipeline implements Serializable {
     String repository_name
     String branch_name
     String git_user
+    String docker_user
     String app_port
     String pr_num
     String dockerTool
@@ -18,6 +19,7 @@ class Pipeline implements Serializable {
         String repository_name,
         String branch_name,
         String git_user,
+        String docker_user,
         String app_port,
         String pr_num,
         String dockerTool,
@@ -27,6 +29,7 @@ class Pipeline implements Serializable {
         this.repository_name = repository_name
         this.branch_name = branch_name
         this.git_user = git_user
+        this.docker_user = docker_user
         this.app_port = app_port
         this.pr_num = pr_num
         this.dockerTool = dockerTool
