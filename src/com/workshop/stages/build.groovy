36d29890d1a8269cf352f92f8deb9b1d6a547c20
Denny Setiawan <dennystw@outlook.com>
2020-07-25 12:16:22 +0700

    Update build.groovy

--- src/com/workshop/stages/build.groovy
+++ src/com/workshop/stages/build.groovy
@@ -9,7 +9,7 @@ def build(Pipeline p) {
 
     docker.withTool("${c.default_docker_jenkins_tool}") {
         docker.withRegistry("${p.docker_registry}", "${c.default_docker_registry_jenkins_cred}") {
-            def image = docker.build("${p.git_user}/${p.repository_name}:build-$BUILD_NUMBER")
+            def image = docker.build("${p.docker_user}/${p.repository_name}:build-$BUILD_NUMBER")
             image.push()
             image.push('latest')
         }
